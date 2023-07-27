5c4b2c78132a5f213e492f36889cf05c01c4f237
Challen Bellamey <challenbellamey@hotmail.com>
2023-07-27 16:27:55 +0100

    Dev message

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -13,6 +13,7 @@ const Head = () => {
     return (
         <div className="headContainer">
             <Title />
+            <span>Note: "Psychology in Seattle Podcast" playlist size seems to be causing issues for the free hosting service. Avoid using it and "All" for now and I will try to find a workaround.</span>
             <SearchTermsInput />
             <PlaylistInput />
             <SearchButton />
